b9fbc22098371e65078c324174c598c25f15f7b9
dev_abir <59115184+mdabirhasan551@users.noreply.github.com>
2024-01-25 11:58:26 +0600

    Initial data Load

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./components/Layout/Main.jsx";
+import OrderReview from "./components/OrderReview/OrderReview.jsx";
+import Home from "./components/Home/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader: () => fetch("tshirt.json"),
+      },
+      {
+        path: "/review",
+        element: <OrderReview />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
